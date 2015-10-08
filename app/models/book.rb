d607ba00bb757296d9947d68def357804f21b653
shivam <shivamgulati1991@gmail.com>
2015-10-07 22:16:52 -0400

    case error

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,7 +9,7 @@ class Book < ActiveRecord::Base
 def self.search(search)
     if search
     	#find(:all, :conditions => ['ISBN LIKE ?', "%#{search}%"])
-    self.where(["Isbn like ? or Author like ? or Description like ? or Title like ? or Status like ?", "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%"])
+    self.where(["isbn like ? or Author like ? or Description like ? or Title like ? or Status like ?", "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%"])
   
   	else
     	self.all
